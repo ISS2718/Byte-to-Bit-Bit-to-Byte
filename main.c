52b0aa1022a3093a5d933ac8f84b63c9261578e0
Isaac Soares <isaacsantos.soares@hotmail.com>
2021-03-04 01:31:33 -0300

    Create main.c
    I paste the code for GitHub

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int data = 0b10100001; // value in binary
+    int bin[8]; // Array for deocompose the byte in bits 
+    int val = 0; // varieble for recompose de value
+    
+    
+    printf("Value for decompose is: %d\n", data);
+    
+    for (char i = 0; i < 8; i++)
+    {
+        bin[i] = 0b00000001 & data >> i;
+        printf("%d\n", bin[i]);
+    }
+    
+    for (char i = 7; i > -1; i--)
+    {
+        val = bin[i] | val << 1;
+    }
+    
+    printf("Value recomposed is: %d\n", val);
+    return 0;
+}
